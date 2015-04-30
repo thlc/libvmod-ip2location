a77e1ac27896ce4d0ad5a1308c7eedb7cafe6ef6
Thomas Lecomte <th.lecomte@gmail.com>
2015-04-30 09:57:07 +0200

    fixed tons of typos and syntax errors.

--- src/vmod_ip2location.c
+++ src/vmod_ip2location.c
@@ -12,14 +12,14 @@
 void
 i2pl_free(void *d)
 {
-  IP2Location_close(((IP2Location *)d)->i2pl_handle);
+  IP2Location_close(((struct vmod_ip2location_data *)d)->ip2l_handle);
 }
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
         struct vmod_ip2location_data *data = calloc(1, sizeof(*data));
-        data->ip2l_handle = IP2LocatioN_open(conf-> ...);
+        data->ip2l_handle = IP2Location_open(IP2LOCATION_DB_PATH);
         priv->priv = data;
         priv->free = i2pl_free;
 	return (0);
@@ -31,8 +31,8 @@ vmod_country_short(struct sess *sp, struct vmod_priv *priv, const char *ip)
 	IP2LocationRecord *r;
 	const char *code;
 
-	r = IP2Location_get_country_short((vmod_ip2location_data *)priv->i2pl_handle, (char *)ip)
-	code = WS_Dup(sp->wrk->ws, r->short_country);
+	r = IP2Location_get_country_short(((struct vmod_ip2location_data *)priv)->ip2l_handle, (char *)ip);
+	code = WS_Dup(sp->wrk->ws, r->country_short);
         IP2Location_free_record(r);
 
 	return (code);
--- src/vmod_ip2location.h
+++ src/vmod_ip2location.h
@@ -1,3 +1,5 @@
 struct vmod_ip2location_data {
   IP2Location	*ip2l_handle; 
-}
+};
+
+#define IP2LOCATION_DB_PATH "/usr/lib/ip2location/IP2LOCATION.BIN"

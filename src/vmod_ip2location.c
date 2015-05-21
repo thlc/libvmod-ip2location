a0a450c37a20791f62aa0eb0a62d08a52d90b0ce
Thomas Lecomte <th.lecomte@gmail.com>
2015-05-21 09:35:21 +0200

    added sanity checks, made reload_db reentrant

--- src/vmod_ip2location.c
+++ src/vmod_ip2location.c
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <IP2Location.h>
 
 #include "vrt.h"
@@ -32,10 +33,16 @@ reload_db(ip2location_data_t *data)
 
   if (stat(IP2LOCATION_DB_PATH, &s) == 0) {
     if (data->ip2l_handle == NULL || s.st_mtime > data->ip2l_db_ts) {
-      if (data->ip2l_handle != NULL)
-        IP2Location_close(data->ip2l_handle);
-      data->ip2l_handle = IP2Location_open(IP2LOCATION_DB_PATH);
-      data->ip2l_db_ts = s.st_mtime;
+      // try to obtain the lock to reload the database.
+      // if we can't, it means another thread is already reloading the database.
+      if (pthread_mutex_trylock(&data->lock) == 0) {
+        if (data->ip2l_handle != NULL)
+          IP2Location_close(data->ip2l_handle);
+        data->ip2l_handle = IP2Location_open(IP2LOCATION_DB_PATH);
+        IP2Location_open_mem(data->ip2l_handle, IP2LOCATION_CACHE_MEMORY);
+        data->ip2l_db_ts = s.st_mtime;
+	pthread_mutex_unlock(&data->lock);
+      }
     }
   }
 }
@@ -48,6 +55,7 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 
         ip2location_data_t *data = calloc(1, sizeof(*data));
         data->ip2l_handle = NULL;
+	pthread_mutex_init(&data->lock, NULL);
         reload_db(data);
         priv->priv = data;
         priv->free = i2pl_free;
@@ -65,12 +73,15 @@ vmod_country_short(struct sess *sp, struct vmod_priv *priv, const char *ip)
         handle = ((ip2location_data_t *)priv->priv)->ip2l_handle;
         if (handle != NULL) {
 		r = IP2Location_get_country_short(handle, (char *)ip);
-		code = WS_Dup(sp->wrk->ws, r->country_short);
-        	IP2Location_free_record(r);
-	} else {
-		VMOD_LOG("ERROR: IP2Location database failed to load");
-		code = WS_Dup(sp->wrk->ws, "-");
+		if (r != NULL) {
+			code = WS_Dup(sp->wrk->ws, r->country_short);
+        		IP2Location_free_record(r);
+			return (code);
+		}
 	}
+
+	VMOD_LOG("ERROR: IP2Location database failed to load");
+	code = WS_Dup(sp->wrk->ws, "-");
 
 	return (code);
 }
@@ -86,12 +97,15 @@ vmod_region(struct sess *sp, struct vmod_priv *priv, const char *ip)
         handle = ((ip2location_data_t *)priv->priv)->ip2l_handle;
         if (handle != NULL) {
 		r = IP2Location_get_region(handle, (char *)ip);
-		code = WS_Dup(sp->wrk->ws, r->region);
-        	IP2Location_free_record(r);
-	} else {
-		VMOD_LOG("ERROR: IP2Location database failed to load");
-		code = WS_Dup(sp->wrk->ws, "-");
+                if (r != NULL) {
+			code = WS_Dup(sp->wrk->ws, r->region);
+        		IP2Location_free_record(r);
+			return (code);
+                }
 	}
+
+	VMOD_LOG("ERROR: IP2Location database failed to load");
+	code = WS_Dup(sp->wrk->ws, "-");
 
 	return (code);
 }
--- src/vmod_ip2location.h
+++ src/vmod_ip2location.h
@@ -1,6 +1,7 @@
 typedef struct vmod_ip2location_data {
-  time_t	ip2l_db_ts;     /* timestamp of the database file */
-  IP2Location	*ip2l_handle;
+  time_t		ip2l_db_ts;     /* timestamp of the database file */
+  IP2Location		*ip2l_handle;
+  pthread_mutex_t	lock;
 } ip2location_data_t;
 
 /* FIXME: put this in a variable/config/default file */

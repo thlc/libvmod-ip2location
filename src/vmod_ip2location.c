5ab7ac6d16fd819a85e5c89d9d5d58ad149d8bfc
Thomas Lecomte <th.lecomte@gmail.com>
2015-12-28 09:20:16 +0100

    migration to varnish 4

--- src/vmod_ip2location.c
+++ src/vmod_ip2location.c
@@ -5,12 +5,13 @@
 #include <pthread.h>
 #include <IP2Location.h>
 
+#include "vcl.h"
 #include "vrt.h"
-#include "bin/varnishd/cache.h"
+#include "cache/cache.h"
 #include "vcc_if.h"
 #include "vmod_ip2location.h"
 
-#define VMOD_LOG(...) WSP(sp, SLT_VCL_Log, __VA_ARGS__);
+#define VMOD_LOG(...) VSLb(ctx->vsl, SLT_VCL_Log, __VA_ARGS__);
 
 void
 i2pl_free(void *d)
@@ -31,6 +32,9 @@ reload_db(ip2location_data_t *data)
 {
   struct stat s;
 
+  if (!data)
+  	WRONG("reload_db: data is NULL");
+
   if (stat(IP2LOCATION_DB_PATH, &s) == 0) {
     if (data->ip2l_handle == NULL || s.st_mtime > data->ip2l_db_ts) {
       // try to obtain the lock to reload the database.
@@ -49,63 +53,81 @@ reload_db(ip2location_data_t *data)
 
 
 int
-init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
+event_handler(VRT_CTX, struct vmod_priv *priv, enum vcl_event_e e)
 {
-	struct stat s;
+	ip2location_data_t *data = NULL;
 
-        ip2location_data_t *data = calloc(1, sizeof(*data));
-        data->ip2l_handle = NULL;
-	pthread_mutex_init(&data->lock, NULL);
-        reload_db(data);
-        priv->priv = data;
-        priv->free = i2pl_free;
+	switch (e) {
+	case VCL_EVENT_LOAD:
+		data = calloc(1, sizeof(*data));
+		if (!data)
+			WRONG("unable to allocate memory for ip2location data structure");
+		data->ip2l_handle = NULL;
+		pthread_mutex_init(&data->lock, NULL);
+		reload_db(data);
+		priv->priv = data;
+		priv->free = i2pl_free;
+        	break;
+	case VCL_EVENT_DISCARD:
+		i2pl_free(priv->priv);
+		break;
+	case VCL_EVENT_WARM:
+	case VCL_EVENT_COLD:
+	case VCL_EVENT_USE:
+	default:
+		break;
+	}
 	return (0);
 }
 
-const char *
-vmod_country_short(struct sess *sp, struct vmod_priv *priv, const char *ip)
+VCL_STRING
+vmod_country_short(VRT_CTX, struct vmod_priv *priv, VCL_STRING ip)
 {
 	IP2LocationRecord *r;
 	IP2Location *handle;
 	const char *code = NULL;
 
+        if (priv->priv == NULL)
+          WRONG("ip2location: priv->priv is NULL");
 	reload_db(priv->priv);
         handle = ((ip2location_data_t *)priv->priv)->ip2l_handle;
         if (handle != NULL) {
 		r = IP2Location_get_country_short(handle, (char *)ip);
 		if (r != NULL) {
-			code = WS_Dup(sp->wrk->ws, r->country_short);
+			code = WS_Copy(ctx->ws, r->country_short, -1);
         		IP2Location_free_record(r);
 			return (code);
 		}
 	}
 
 	VMOD_LOG("ERROR: IP2Location database failed to load");
-	code = WS_Dup(sp->wrk->ws, "-");
+	code = WS_Copy(ctx->ws, "-", -1);
 
 	return (code);
 }
 
-const char *
-vmod_region(struct sess *sp, struct vmod_priv *priv, const char *ip)
+VCL_STRING
+vmod_region(VRT_CTX, struct vmod_priv *priv, VCL_STRING ip)
 {
 	IP2LocationRecord *r;
 	IP2Location *handle;
 	const char *code = NULL;
 
+        if (priv->priv == NULL)
+          WRONG("ip2location: priv->priv is NULL");
 	reload_db(priv->priv);
         handle = ((ip2location_data_t *)priv->priv)->ip2l_handle;
         if (handle != NULL) {
 		r = IP2Location_get_region(handle, (char *)ip);
                 if (r != NULL) {
-			code = WS_Dup(sp->wrk->ws, r->region);
+			code = WS_Copy(ctx->ws, r->region, -1);
         		IP2Location_free_record(r);
 			return (code);
                 }
 	}
 
 	VMOD_LOG("ERROR: IP2Location database failed to load");
-	code = WS_Dup(sp->wrk->ws, "-");
+	code = WS_Copy(ctx->ws, "-", -1);
 
 	return (code);
 }
